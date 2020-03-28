849d904060eab62f10495e1c6272f1fa1cba90cd
Gurjeet Singh <singh@gurjeet.co.in>
2020-03-28 13:02:28 +0530

    blank

---
+++ src/FPDFBarcode/FPDFBarcode.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sarabitcom\FPDFBarcode;
+
+/**
+ * Description of FpdfBarCode
+ *
+ * @author gurjeet
+ */
+class FPDFBarcode {
+
+    public function getTestRandom() {
+        return rand(100000, 999999);
+    }
+
+}
