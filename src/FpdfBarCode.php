8613bedce59de4891fa039eb100f012928fbe146
Gurjeet Singh <singh@gurjeet.co.in>
2020-03-28 12:50:03 +0530

    blank

---
+++ src/FpdfBarCode.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of FpdfBarCode
+ *
+ * @author gurjeet
+ */
+class FpdfBarCode {
+
+    public function getTestRandom() {
+        return rand(100000, 999999);
+    }
+
+}
